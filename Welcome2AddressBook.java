7beeb28f384f4432fad16baa0f73f2d8633923ce
pooj1277 <pooja.sd3@gmail.com>
2021-10-05 23:44:42 -0700

    [Added]:Welcome to the Address Book Program

---
+++ Welcome2AddressBook.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.addressBook;
+
+public class Welcome2AddressBook {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Address Book Program");
+	}
+
+}
